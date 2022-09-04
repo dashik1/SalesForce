940a34aa6e3d7464bd016e34a358e1346c7d0fed
Dasha <darya.pashyk@gmail.com>
2022-09-04 21:29:57 +0400

    HW_15

--- src/test/java/constants/Urls.java
+++ src/test/java/constants/Urls.java
@@ -2,7 +2,7 @@ package constants;
 
 public class Urls {
 
-    public static final String LOGIN = "https://mycompany-8d.my.salesforce.com";
+    public static final String LOGIN = "https://forcecom-30a.my.salesforce.com";
     public static final String ACCOUNTS = "/lightning/o/Account/list?filterName=Recent";
 
 }
--- src/test/java/pages/AccountsPage.java
+++ src/test/java/pages/AccountsPage.java
@@ -1,11 +1,15 @@
 package pages;
 
 import constants.Urls;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class AccountsPage extends BasePage {
+
+    private static final Logger LOGGER = LogManager.getLogger(AccountsPage.class.getName());
 
     @FindBy(xpath = "//div[@title='New']")
     private WebElement newButton;
@@ -16,11 +20,13 @@ public class AccountsPage extends BasePage {
     }
 
     public AccountsPage openAccountsPage() {
+        LOGGER.debug(String.format("Attempt to open Account page by url: %s", Urls.LOGIN.concat(Urls.ACCOUNTS)));
         driver.get(Urls.LOGIN.concat(Urls.ACCOUNTS));
         return this;
     }
 
     public NewAccountModalPage openNewAccountModal() {
+        LOGGER.debug(String.format("Attempt to click to %s button", newButton));
         newButton.click();
         return new NewAccountModalPage(driver);
     }
